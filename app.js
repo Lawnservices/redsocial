dfdc428f7a028149e7155ab3d321737d49ba5130
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-01-16 16:20:08 -0500

    fix

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.g
 import { getFirestore, doc, setDoc, getDoc, collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 
-const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET",
-  messagingSenderId: "ID",
-  appId: "APP_ID"
+ const firebaseConfig = {
+  apiKey: "TU_API_KEY",                       // de tu proyecto Firebase
+  authDomain: "likes-web-1992b.firebaseapp.com", // tu dominio Firebase Auth
+  projectId: "likes-web-1992b",               // Project ID
+  storageBucket: "TU_BUCKET",                 // ej: likes-web-1992b.appspot.com
+  messagingSenderId: "TU_MESSAGING_ID",       // de Firebase
+  appId: "TU_APP_ID"                          // de Firebase
 };
 
 const app = initializeApp(firebaseConfig);
